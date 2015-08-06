b736de03b203e5fd83e1fe04501aeceb6057b507
Tobbe Gyllebring <tobbe@drunkcod.com>
2015-08-06 23:35:53 +0200

    Specs project added. WorkQueue configured via messageType.

--- SimpleWorkQueue/Program.cs
+++ SimpleWorkQueue/Program.cs
@@ -5,27 +5,49 @@ using Drunkcod.Data.ServiceBroker;
 
 namespace SimpleWorkQueue
 {
+	class ServiceBrokerWorkQueue
+	{
+		readonly SqlServerServiceBroker broker;
+		readonly ServiceBrokerQueue workQueue;
+		readonly ServiceBrokerMessageType workItemMessageType;
+		readonly ServiceBrokerService sinkService;
+		readonly ServiceBrokerService workerService;
+		readonly ServiceBrokerContract workQueueItems;
+
+		public ServiceBrokerWorkQueue(SqlServerServiceBroker broker, string messageType) {
+			this.broker = broker;
+			broker.EnableBroker();
+			sinkService = broker.CreateSinkService();
+
+			workItemMessageType = broker.CreateMessageType(messageType);
+			workQueue = broker.CreateQueue(messageType + "Queue");
+			workQueueItems = broker.CreateContract(messageType + "Contract", workItemMessageType);
+			workerService = workQueue.CreateService(messageType + "Service", workQueueItems);
+		}
+
+		public void Post(string workItem) {
+			var conversation = broker.BeginConversation(sinkService, workerService, workQueueItems);
+			conversation.Send(workItemMessageType, Encoding.UTF8.GetBytes(workItem));
+		}
+
+		public bool Receive(Action<string> handleWork) {
+			return workQueue.Receive((c, type, body) => {
+				handleWork(new StreamReader(body).ReadToEnd());
+				c.EndConversation();
+			});
+		}
+	}
+
 	class Program
 	{
 		static void Main(string[] args) {
-			var sbs = new SqlServerServiceBroker("Server=.;Integrated Security=SSPI;Initial Catalog=WorkWork");
+			var workQueue = new ServiceBrokerWorkQueue(new SqlServerServiceBroker("Server=.;Integrated Security=SSPI;Initial Catalog=WorkWork"), "WorkItem");
 
-			sbs.EnableBroker();
-			var workItem = sbs.CreateMessageType("WorkItem");
-			var workQueue = sbs.CreateQueue("WorkQueue");
+			workQueue.Post("Hello World!");
+			workQueue.Post("Hello World!!");
 
-			var workQueueItems = sbs.CreateContract("WorkQueueItems", workItem);
-
-			var sinkService = sbs.CreateSinkService();
-			var workerService = workQueue.CreateService("WorkerService", workQueueItems);
-
-			var conversation = sbs.BeginConversation(sinkService, workerService, workQueueItems);
-			conversation.Send(workItem, Encoding.UTF8.GetBytes("Hello Service Broker World!"));
-
-			while(workQueue.Receive((c, type, body) => {
-				Console.WriteLine("{0}: {1}", type.Name, new StreamReader(body).ReadToEnd());
-				c.EndConversation();
-			}));
+			while(workQueue.Receive(Console.WriteLine))
+				;
 		}
 	}
 }
